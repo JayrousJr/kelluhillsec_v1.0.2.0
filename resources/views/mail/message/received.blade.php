036a06e3594276ad5b13ca9906814599cd682dcb
Joshua Jayrous <joshuajayrous@gmail.com>
2024-02-09 19:46:58 +0300

    Update received.blade.php

--- resources/views/mail/message/received.blade.php
+++ resources/views/mail/message/received.blade.php
@@ -1,10 +1,11 @@
 <x-mail::message>
     Hello dear <strong>{{$name}}</strong>,<br>
     Your Message has been sent successiful!<br><br>
-    Thank You for contacting {{ config('app.name') }}.
+    Be a great Customer by letting people know of our service at {{ config('app.name') }}.<br>
 
     <x-mail::button :url="'https://www.kellusec.ac.tz'">
-        Go to The Site
+        Go to The WebSite
     </x-mail::button>
 
+    Thank You for contacting {{ config('app.name') }}.
 </x-mail::message>

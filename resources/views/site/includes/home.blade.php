8851755bb9d6069658a7b40bfcf66ad82a7695b5
Joshua Jayrous <joshuajayrous@gmail.com>
2024-02-18 08:55:11 +0300

    Meta tag Commit

--- resources/views/site/includes/home.blade.php
+++ resources/views/site/includes/home.blade.php
@@ -5,7 +5,11 @@
     <title>Home | Kellu Hill School</title>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="description" content="Elearn project">
+    <meta name="description" content="Welcome to Kellu Hill Secondary School, where excellence meets education.">
+    <meta name="description"
+        content="Discover our academic programs, extracurricular activities, and supportive community. Enroll today!">
+    <meta name="description"
+        content="This Platform has been designed and Developed by CLOUDS9 TECH http:://cloudstechn.com">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" href="/assets/styles/bootstrap4/bootstrap.min.css">
     <link href="/assets/plugins/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
